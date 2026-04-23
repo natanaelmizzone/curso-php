c839bb5626495cad437bc55c0695c42a90822fb9
Natanael Mizzone <natanmizzone@gmail.com>
2026-04-22 22:13:34 -0300

    orientação
    - índice (0,1,2...) para uma forma melhor de verificação.

--- 06_array.php
+++ 06_array.php
@@ -2,6 +2,15 @@
 //$carros = ['Uno', 'Gol','Fusion', 'Civic'];
 //echo $carros[2];
 
-$carroUm = ['Uno', 'Fiat', 'Branco'];
-echo $carroUm[2];
+//$carroUm = ['Uno', 'Fiat', 'Branco'];
+//echo $carroUm[2];
+$carro_um = [
+'modelo' => 'Uno',
+'marca' => 'Fiat',
+'cor' => 'Branco',
+];
+
+echo $carro_um['cor'];
+echo $carro_um['modelo'];
+echo $carro_um['marca'];
 ?>
