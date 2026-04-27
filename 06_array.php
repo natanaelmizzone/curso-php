8ba280a6d9c8d21be25de5c0105f56be182ce382
Natanael Mizzone <natanmizzone@gmail.com>
2026-04-27 20:15:31 -0300

    Array
    - Trabalhando com array e imprimindo as variaveis.

--- 06_array.php
+++ 06_array.php
@@ -4,13 +4,63 @@
 
 //$carroUm = ['Uno', 'Fiat', 'Branco'];
 //echo $carroUm[2];
+
 $carro_um = [
-'modelo' => 'Uno',
-'marca' => 'Fiat',
-'cor' => 'Branco',
+'modelo' => 'Vectra',
+'marca' => 'Chevrolet',
+'cor' => 'Prata',
+'ano' => '2010',
+'tipo' => 'Hatch',
 ];
 
 echo $carro_um['cor'];
+echo '<br>';
 echo $carro_um['modelo'];
+echo '<br>';
 echo $carro_um['marca'];
+echo '<br>';
+echo $carro_um['tipo'];
+echo '<br>';
+echo $carro_um['ano'];
+echo '<br><br>';
+
+//ATIVIDADE EM SALA DE AULA 02
+
+$carro_dois = [
+'modelo' => 'Punto',
+'marca' => 'Fiat',
+'cor' => 'Branco',
+'ano' => '2011',
+'tipo' => 'Hacth'
+];
+
+echo $carro_dois['cor'];
+echo '-';
+echo $carro_dois['modelo'];
+echo '-';
+echo $carro_dois['marca'];
+echo '-';
+echo $carro_dois['tipo'];
+echo '-';
+echo $carro_dois['ano'];
+echo '<br><br>';
+
+//ATIVIDADE EM SALA DE AULA 03
+
+$carro_tres = [
+'modelo' => '911',
+'marca' => 'Porshe',
+'cor' => 'Cinza',
+];
+
+
+echo 'Marca: ', $carro_tres['modelo'], '<br>';
+echo 'Marca: ', $carro_tres['marca'], '<br>';
+echo 'Marca: ', $carro_tres['cor'], '<br>';
+
+/*
+SEMPRE QUE PRECISAR DEIXAR UM LEMBRETE, CASO FALTE TERMINAR UMA ATIVIDADE OU ALGO CORRELATIVO.
+*/
+
 ?>
+
