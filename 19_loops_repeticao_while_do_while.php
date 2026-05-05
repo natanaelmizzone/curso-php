e061ad18c5ef417effd4459704055f59e746667b
Natanael Mizzone <natanmizzone@gmail.com>
2026-05-05 19:36:07 -0300

    utilizando o DO_WHILE

--- 19_loops_repeticao_while_do_while.php
+++ 19_loops_repeticao_while_do_while.php
@@ -1,8 +1,15 @@
 <?php
-$a = 9;
-
+$a = 11; /*WHILE utiliza ENQUANTO*/
 while ($a <= 10) {
     echo $a . "<br>";
     $a++;
 }
+
+echo '<hr>';
+
+$a = 11; /*O DO e WHILE é faça pelo menos uma vez*/
+do {
+    echo $a;
+    $a++;
+} while ($a <= 10);
 ?>
