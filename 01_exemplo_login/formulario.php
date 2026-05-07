21dda89b3328bf890e3ec8bbdaef8ae83ba10747
Natanael Mizzone <natanmizzone@gmail.com>
2026-05-06 22:20:34 -0300

    Login e senha
    POST;
    TYPE Password
    TYPE Email

---
+++ 01_exemplo_login/formulario.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>login</h1>
+    <form action="validar_login.php" method="post">
+        <label>E-mail: </label>
+        <input type="email" name="email"><br>
+        <label>Senha:</label>
+        <input type="password" name="senha"><br>
+        <button type="submit">Acessar</button>
+        <button type="reset">Limpar</button>
+    </form>
+</body>
+</html>
